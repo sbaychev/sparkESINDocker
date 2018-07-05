7e9147eca728a3a432f607774be6441059c2fce9
Stefan Baychev <sbaychev@gmail.com>
2018-07-05 17:42:57 +0100

    Update windows_start_es.bat

--- elasticsearch/windows_start_es.bat
+++ elasticsearch/windows_start_es.bat
@@ -3,5 +3,8 @@ docker rm es
 docker rmi sb/elasticsearch
 
 docker build -f Dockerfile -t sb/elasticsearch .
-docker create --name es -p 9200:9200 -p 9300:9300 sb/elasticsearch
+docker volume create --name esdata
+
+docker create --name es -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -e "xpack.security.enabled=false" --log-opt max-size=50m --log-opt max-file=10 -v esdata:/usr/share/elasticsearch/data -v esdata:/usr/share/elasticsearch/logs sb/elasticsearch
+
 docker start es
