dd1039a3c9cbcadec3cd3cb4afe1511d23407593
Stefan Baychev <sbaychev@gmail.com>
2018-07-10 16:06:53 +0100

    Update SparkDriver.java

--- src/main/java/driver/SparkDriver.java
+++ src/main/java/driver/SparkDriver.java
@@ -1,5 +1,7 @@
 package driver;
 
+import static java.lang.String.format;
+
 import data.Episodes;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
@@ -24,8 +26,17 @@ public class SparkDriver {
             .setMaster("local[*]");
 
         conf.set("es.nodes", "localhost:9200");
-        conf.set("es.nodes.discovery", "true");
-        conf.set("es.nodes.wan.only", "false");
+
+        //Whether to discover the nodes within the ElasticSearch cluster or only to use the ones given
+        conf.set("es.nodes.discovery", "false");
+
+        // In this mode, the connector disables discovery and only connects through the declared es.nodes during all operations,
+        // including reads and writes. Note that in this mode, performance is highly affected
+        conf.set("es.nodes.wan.only", "true");
+
+        //if we want based on the .avro file what we get to be what spark automatically pushes to ES
+        conf.set("es.index.auto.create", "true");
+
         conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
         conf.set("spark.kryo.registrator", CustomKryoRegistrator.class.getName());
         conf.set("spark.kryoserializer.buffer", "128m");
@@ -50,12 +61,12 @@ public class SparkDriver {
                 String air_date = row.getString(1);
                 int doctor = row.getInt(2);
 
-                LOGGER.info("title: " + title + " air_date: " + air_date + " doctor: " + doctor);
+                LOGGER.info(format("title: %s air_date: %s doctor: %d", title, air_date, doctor));
 
                 return new Episodes(title, air_date, doctor);
             });
 
-        LOGGER.info("Number of Row Lines | Records submitted to ES: " + episodesJavaRDD.count());
+        LOGGER.info(format("Number of Row Lines | Records submitted to ES: %d", episodesJavaRDD.count()));
 
         JavaEsSpark.saveToEs(episodesJavaRDD, "episodes/the-doctors");
 
